597a18ec6451ddf517fe9a3264bc074eff834bfc
Maddie Davidson <zerios042@gmail.com>
2024-12-09 14:05:59 -0500

    Added a header along with imports for our program.

---
+++ plotting.py
@@ -0,0 +1,10 @@
+"""
+@date: 12/9/24
+@author: Madison Davidson, Casie Peng
+@PID: mzddie, casiepeng
+@assignment: Class Project
+"""
+import sys, requests
+from scapy.all import traceroute
+from gmplot import gmplot
+from scapy.layers.inet import socket
